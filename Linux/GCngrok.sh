c712e40534c3bdefeffe1f540c3fb38d00b3fa22
Bangunaisas <95173321+Bangunaisas@users.noreply.github.com>
2021-11-28 20:08:02 +0700

    Create GCngrok.sh

---
+++ Linux/GCngrok.sh
@@ -0,0 +1,8 @@
+rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "========================"
+echo "ลง ngrok"
+echo "========================"
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+read -p "วาง ngrok token ดิ: " CRP
+./ngrok authtoken $CRP
